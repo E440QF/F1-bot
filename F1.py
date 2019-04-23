fabb932022c5e376e9594551cd83dd9b9edd8c35
nicola <ettore.beltra@gmail.com>
2019-04-23 11:11:07 +0200

    added calendar support, kind of

--- F1.py
+++ F1.py
@@ -1,12 +1,15 @@
 import requests
 import urllib.request
 import time
+import datetime
 import re
 from bs4 import BeautifulSoup
+
 def getHtml(url,tag):
 	response = requests.get(url)
 	soup = BeautifulSoup(response.text, "html.parser")
 	return(soup.findAll(tag))
+
 def getResults(n,url):
 	pilots = getHtml(url,'tr')
 	dataPattern  = r">.+<"
@@ -35,6 +38,7 @@ def getResults(n,url):
 ######################################################
 	result      = {"pos":pos,"carNumber":carNumber,"name":name,"surname":surname,"abbr":abbr,"team":team,"laps":laps,"time":time,"pts":pts}
 	return(result)
+
 def getResultsUrl(state):
 	urlPattern = r"\".{75,95}\""
 	racesUrl = "https://www.formula1.com/en/racing/2019/"+state+".html"
@@ -43,8 +47,25 @@ def getResultsUrl(state):
 	rUrlRE = re.search(urlPattern,race[9])
 	rUrl = rUrlRE.group()
 	return(rUrl.replace("\"",""))
+
 #def getRace(n)
 #print(getHtml('https://www.formula1.com/en/racing/2019.html','article')[0])
 #print(getResults(1,getResultsUrl("China")))
 
+def monthToNumber(month):
+	months = {"Jan":1,"Feb":2,"Mar":3,"Apr":4,"May":5,"Jun":6,"Jul":7,"Aug":8,"Sep":9,"Oct":10,"Nov":11,"Dec":12}
+	return(months[month])
 
+statePattern1  = r"[0-9]{4,4}/[A-Za-z]+/_jcr"
+statePattern2 = r"[A-Za-z]+"
+racesList = getHtml("https://www.formula1.com/en/racing/2019.html","article")
+raceAttr = str(racesList[1]).split("\n")
+print(raceAttr[12])
+state1 = re.search(statePattern1,raceAttr[1])
+state2 = re.search(statePattern2,state1.group())
+#print(state2.group())
+monthPattern = r"[A-Z][a-z][a-z]"
+now = datetime.datetime.now()
+month = re.search(monthPattern,raceAttr[12])
+print(now.month)
+print(monthToNumber(month.group()))
